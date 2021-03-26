2284c001221e32f71bd80ef1e91fb58a69b5090b
Levi Daymon <levithehandyman@outlook.com>
2021-03-26 14:06:57 -0400

    working on api calls

---
+++ src/GitHub.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class GitHub extends Component {
+    constructor(props) {
+        super(props);
+        this.getGitHubData('greg');
+    }
+
+    getGitHubData (_searchTerm) {
+        axios.get("https://api.github.com.search/users?q="+_searchTerm)
+        .then(res => {
+            console.log(res.data.items);
+        });
+    }
+
+    render() {
+        return(
+            <div>
+            </div>
+        );
+    }
+}
+
+export default GitHub;
